0e02af4e9766b3e439b49114ed5cd65d2b3b2bab
Zachery Gentry <zacherygentry@gmail.com>
2022-09-03 12:26:32 -0500

    wip

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,109 +9,183 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomeScreenState extends State<HomeScreen> {
+  int? playerRoll;
+  int? computerRoll;
 
-  void _incrementCounter() {
+  void playerRollDice() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      playerRoll = Random().nextInt(6) + 1;
     });
+  }
+
+  void computerRollDice() {
+    setState(() {
+      computerRoll = Random().nextInt(6) + 1;
+    });
+  }
+
+  List<List<int?>> playerGameState = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+  ];
+
+  List<List<int?>> computerGameState = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+  ];
+
+  void addPlayerRoll(int roll, int column) {
+    for (int i = 0; i < 3; i++) {
+      if (playerGameState[i][column] == null) {
+        setState(() {
+          playerGameState[i][column] = roll;
+        });
+        break;
+      }
+    }
+  }
+
+  void addComputerRoll(int roll, int column) {
+    for (int i = 0; i < 3; i++) {
+      if (computerGameState[i][column] == null) {
+        setState(() {
+          computerGameState[i][column] = roll;
+        });
+        break;
+      }
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            Expanded(
+              child: Center(
+                child: Column(
+                  children: [
+                    Text("Opponent"),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        for (int i = 0; i < 3; i++)
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              for (int j = 0; j < 3; j++)
+                                Container(
+                                  width: 50,
+                                  height: 50,
+                                  color: Colors.grey,
+                                  child: Center(
+                                    child: Text(
+                                      computerGameState[j][i] == null
+                                          ? ""
+                                          : computerGameState[j][i].toString(),
+                                    ),
+                                  ),
+                                ),
+                              ElevatedButton(
+                                onPressed: () {
+                                  // use column index to add roll to playerGameState
+                                  addComputerRoll(playerRoll!, i);
+                                },
+                                child: Text("Add here"),
+                              ),
+                            ],
+                          ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Expanded(
+              child: Center(
+                child: Column(
+                  children: [
+                    const Text("Active Player"),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        for (int i = 0; i < 3; i++)
+                          Column(
+                            children: [
+                              for (int j = 0; j < 3; j++)
+                                Container(
+                                  width: 50,
+                                  height: 50,
+                                  color: Colors.grey,
+                                  child: Center(
+                                    child: Text(
+                                      playerGameState[j][i] == null
+                                          ? ""
+                                          : playerGameState[j][i].toString(),
+                                    ),
+                                  ),
+                                ),
+                              ElevatedButton(
+                                onPressed: () {
+                                  // use column index to add roll to playerGameState
+                                  addPlayerRoll(playerRoll!, i);
+                                },
+                                child: Text("Add here"),
+                              ),
+                            ],
+                          ),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: playerRollDice,
+                      child: const Text("Roll"),
+                    ),
+                    const SizedBox(height: 20),
+                    Container(
+                      height: 50 * 1.33,
+                      width: 50 * 1.33,
+                      decoration: BoxDecoration(
+                        border: Border.all(),
+                      ),
+                      child: Center(
+                        child: Text(
+                          playerRoll?.toString() ?? '',
+                          style: const TextStyle(fontSize: 50),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
